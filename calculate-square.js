969d699e6a5201d27a1225fc791bf81b86f1d4fd
Bohyun Suh <bhsuhkr@naver.com>
2022-12-15 13:15:39 -0600

    callback test

---
+++ calculate-square.js
@@ -0,0 +1,10 @@
+function calculateSquare(number, callback) {
+    setTimeout(function() {
+        if (typeof number !== 'number') {
+            callback(new Error('Argument of type number is expected'));
+            return;
+        }
+        const result = number * number;
+        callback(null, result);
+    }, 1000);
+}
